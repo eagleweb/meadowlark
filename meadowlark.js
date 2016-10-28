e8d3cb3b1306dbaed279d0fbb356cee793995c46
eagleweb <eagleweb@mail.ru>
2016-10-28 16:29:08 +0300

    Update 28.10.16

--- meadowlark.js
+++ meadowlark.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars')
+    .create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
 // пользовательская страница 404
-app.use(function(req, res){
-    res.type('text/plain');
+app.use(function(req, res, next){
     res.status(404);
-    res.send('404 — Не найдено');
+    res.send('404');
 });
 // пользовательская страница 500
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 — Ошибка сервера');
+    res.send('500');
 });
 app.listen(app.get('port'), function(){
     console.log( 'Express запущен на http://localhost:' +
