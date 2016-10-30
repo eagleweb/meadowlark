b7369d4559207f8677499e78b3d022583234d20c
eagleweb <eagleweb@mail.ru>
2016-10-30 15:33:05 +0200

    Update 30.10.16 страница 80 книги

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,5 @@
+var fortune = require('./lib/fortune.js');
+
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars')
@@ -6,20 +8,38 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res){
-    res.render('about');
+    res.render('about', {fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+    });
 });
 
-// пользовательская страница 404
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
+// Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function(req, res, next){
     res.status(404);
     res.send('404');
 });
-// пользовательская страница 500
+// Обработчик ошибки 500 (промежуточное ПО)
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500);
